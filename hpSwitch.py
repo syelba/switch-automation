5997bf715a000e695129405c85dfbc3788d48da4
syelba <160588882+syelba@users.noreply.github.com>
2025-02-25 11:11:14 +0200

    Update hpSwitch.py

--- hpSwitch.py
+++ hpSwitch.py
@@ -1,18 +1,5 @@
 from netmiko import ConnectHandler
 
-setTime = [
-    'configure',
-    'time',
-    'sntp',
-    'sntp server priority 1 10.128.12.31',
-    'sntp server priority 2 10.128.12.32',
-    'show sntp',
-    'timesync sntp',
-    'sntp unicast',
-    'time'
-]
-
-
 
 
 class ConfigSwitch():
@@ -32,6 +19,17 @@ class ConfigSwitch():
 
 
     def setSNTP(self):
+        setTime = [
+            'configure',
+            'time',
+            'sntp',
+            'sntp server priority 1 10.128.12.31',
+            'sntp server priority 2 10.128.12.32',
+            'show sntp',
+            'timesync sntp',
+            'sntp unicast',
+            'time'
+        ]
         # Establish the connection
         # Use ** to unpack the dictionary into keyword arguments for ConnectHandler
         net_connect = ConnectHandler(**self.hp_switch)
@@ -47,7 +45,15 @@ class ConfigSwitch():
             net_connect.disconnect()
             return e
 
-    def setSNMPV3(self):
+    def setSNMPV3(self, user, sha, password):
+        configSNMPV3 = [
+            'configure',
+            'no snmpv3 enable',  # Reset SNMPv3
+            'snmpv3 enable',  # Enable SNMPv3
+            f'snmpv3 user {user} auth sha {sha} priv AES128 {password}'  # Create SNMPv3 user
+            'show snmpv3 user',  # Verify configuration
+            'snmpv3 restricted-access'
+        ]
         # Establish the connection
         # Use ** to unpack the dictionary into keyword arguments for ConnectHandler
         net_connect = ConnectHandler(**self.hp_switch)
@@ -56,7 +62,7 @@ class ConfigSwitch():
         # Enter configuration mode
         net_connect.config_mode()
         try:
-            output = net_connect.send_config_set(setTime)
+            output = net_connect.send_config_set(configSNMPV3)
             net_connect.disconnect()
             return output
         except Exception as e:
