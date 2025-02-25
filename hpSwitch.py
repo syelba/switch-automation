b5a5b328b78fbab1919e4a7d9f698bd286304c42
syelba <160588882+syelba@users.noreply.github.com>
2025-02-25 10:54:16 +0200

    Update hpSwitch.py
    updated class to added commends outside the function

--- hpSwitch.py
+++ hpSwitch.py
@@ -1,39 +1,5 @@
 from netmiko import ConnectHandler
-import time
 
-# Define the device to connect to
-hp_switch = {
-    'device_type': 'hp_procurve',  # Device type for HP ProCurve/ArubaOS switches
-    'host': '10.12.212.120',  # IP address of the switch
-    'username': 'username',  # Username to login
-    'password': 'password',  # Password to login
-    'port': 22,  # SSH port (default is 22)
-}
-
-ilsManaged = ['10.12.183.32', '10.12.188.252', '10.12.188.240', '10.12.185.241', '10.12.185.253', '10.12.177.252',
-              '10.12.177.251', '10.12.186.253', '10.12.186.84', '10.12.186.16', '10.12.184.188', '10.12.189.59',
-              '10.12.189.80', '10.12.183.44', '10.12.189.106','10.12.189.4'
-              ]
-
-
-def configHP(hp_switch, configuration):
-    # Establish the connection
-    # Use ** to unpack the dictionary into keyword arguments for ConnectHandler
-    net_connect = ConnectHandler(**hp_switch)
-    # Enter enable mode (if necessary)
-    net_connect.enable()
-    # Enter configuration mode
-    net_connect.config_mode()
-    try:
-        output = net_connect.send_config_set(setTime)
-        net_connect.disconnect()
-        return output
-    except Exception as e:
-        net_connect.disconnect()
-        return e
-
-
-# Execute a configuration command
 setTime = [
     'configure',
     'time',
@@ -46,17 +12,53 @@ setTime = [
     'time'
 ]
 
-# # Iterate over the list of IP addresses
-# for ip in ilsManaged:
-#     # Update the host in the base configuration
-#     hp_switch = hp_switch.copy()
-#     hp_switch['host'] = '10.12.183.32'
-#     configHP(ip,setTime)
-for ip in ilsManaged:
-    hp_switch = hp_switch.copy()
-    print(ip)
-    hp_switch['host'] = ip
-    try:
-        print(configHP(hp_switch,setTime))
-    except Exception as e:
-        print(e)
+
+
+
+class ConfigSwitch():
+
+    def __init__(self, ip, username, password):
+        self.ip = ip
+        self.username = username
+        self.password = password
+
+        self.hp_switch = {
+            'device_type': 'hp_procurve',  # Device type for HP ProCurve/ArubaOS switches
+            'host': f'{self.ip}',  # IP address of the switch
+            'username': f'{self.username}',  # Username to login
+            'password': f'{self.password}',  # Password to login
+            'port': 22,  # SSH port (default is 22)
+        }
+
+
+    def setSNTP(self):
+        # Establish the connection
+        # Use ** to unpack the dictionary into keyword arguments for ConnectHandler
+        net_connect = ConnectHandler(**self.hp_switch)
+        # Enter enable mode (if necessary)
+        net_connect.enable()
+        # Enter configuration mode
+        net_connect.config_mode()
+        try:
+            output = net_connect.send_config_set(setTime)
+            net_connect.disconnect()
+            return output
+        except Exception as e:
+            net_connect.disconnect()
+            return e
+
+    def setSNMPV3(self):
+        # Establish the connection
+        # Use ** to unpack the dictionary into keyword arguments for ConnectHandler
+        net_connect = ConnectHandler(**self.hp_switch)
+        # Enter enable mode (if necessary)
+        net_connect.enable()
+        # Enter configuration mode
+        net_connect.config_mode()
+        try:
+            output = net_connect.send_config_set(setTime)
+            net_connect.disconnect()
+            return output
+        except Exception as e:
+            net_connect.disconnect()
+            return e
